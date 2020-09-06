d33fd1ecc8c37898070fc63019e4cc3c69488c1f
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-06 21:46:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 # Add your code here
 class Dog 
-
+<<<<<<< HEAD
 attr_accessor :name 
    @@all = []
 def initialize(name)
@@ -24,7 +24,3 @@ def save
   @@all << self 
  end 
 end
-
-def 
-  
-end 
